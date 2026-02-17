479026567d66c77e4bccffe53732b7bfd85aa567
TZGaming <tomzuidervaart@gmail.com>
2026-02-17 12:28:17 +0100

    Eerste push, basis website

--- server.js
+++ server.js
@@ -1,92 +1,91 @@
 import express from 'express'
+import { Liquid } from 'liquidjs'
 
-import { Liquid } from 'liquidjs';
-
-
-// Vul hier jullie team naam in
-const teamName = '';
-
-
+const teamName = 'Radiant' 
 const app = express()
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
-const engine = new Liquid();
-app.engine('liquid', engine.express()); 
-
+const engine = new Liquid()
+app.engine('liquid', engine.express())
 app.set('views', './views')
-
-app.use(express.urlencoded({extended: true}))
-
-
-app.get('/', async function (request, response) {
-
-  // Filter eerst de berichten die je wilt zien, net als bij personen
-  // Deze tabel wordt gedeeld door iedereen, dus verzin zelf een handig filter,
-  // bijvoorbeeld je teamnaam, je projectnaam, je person ID, de datum van vandaag, etc..
-  const params = {
-    'filter[for]': `Team ${teamName}`,
-  }
-
-  // Maak hiermee de URL aan, zoals we dat ook in de browser deden
-  const apiURL = 'https://fdnd.directus.app/items/messages?' + new URLSearchParams(params)
-
-  // Laat eventueel zien wat de filter URL is
-  // (Let op: dit is _niet_ de console van je browser, maar van NodeJS, in je terminal)
-  // console.log('API URL voor messages:', apiURL)
-
-  // Haal daarna de messages data op
-  const messagesResponse = await fetch(apiURL)
-
-  // Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
-  const messagesResponseJSON = await messagesResponse.json()
-
-  // Controleer eventueel de data in je console
-  // console.log(messagesResponseJSON)
-
-  // En render de view met de messages
-  response.render('index.liquid', {
-    teamName: teamName,
-    messages: messagesResponseJSON.data
-  })
-})
-
-app.post('/', async function (request, response) {
-
-  // Stuur een POST request naar de messages tabel
-  // Een POST request bevat ook extra parameters, naast een URL
-  await fetch('https://fdnd.directus.app/items/messages', {
-
-    // Overschrijf de standaard GET method, want ook hier gaan we iets veranderen op de server
-    method: 'POST',
-
-    // Geef de body mee als JSON string
-    body: JSON.stringify({
-      // Dit is zodat we ons bericht straks weer terug kunnen vinden met ons filter
-      for: `Team ${teamName}`,
-      // En dit zijn onze formuliervelden
-      from: request.body.from,
-      text: request.body.text
-    }),
-
-    // En vergeet deze HTTP headers niet: hiermee vertellen we de server dat we JSON doorsturen
-    // (In realistischere projecten zou je hier ook authentication headers of een sleutel meegeven)
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8'
-    }
-  });
-
-  // Stuur de browser daarna weer naar de homepage
-  response.redirect(303, '/')
-})
-
-
 app.set('port', process.env.PORT || 8000)
 
-if (teamName == '') {
-  console.log('Voeg eerst de naam van jullie team in de code toe.')
-} else {
-  app.listen(app.get('port'), function () {
-    console.log(`Application started on http://localhost:${app.get('port')}`)
-  })
-}
+const API_URL = 'https://fdnd.directus.app/items'
+
+// ---------------------------------------------------------
+// GET / : Haalt studenten en berichten op
+// ---------------------------------------------------------
+app.get('/', async function (request, response) {
+  try {
+    const personParams = new URLSearchParams({
+      'fields': '*,squads.squad_id.name',
+      'filter[squads][squad_id][cohort][_eq]': '2526',
+      'filter[squads][squad_id][name][_in]': '1I,1J',
+      'deep[squads][filter][squad_id][name][_in]': '1I,1J'
+    });
+
+    const messageParams = new URLSearchParams({
+      'filter[for]': `Team ${teamName}`,
+      'sort': '-date_created',
+      'limit': 15
+    });
+
+    const [personRes, messageRes] = await Promise.all([
+      fetch(`${API_URL}/person?${personParams.toString()}`),
+      fetch(`${API_URL}/messages?${messageParams.toString()}`)
+    ]);
+
+    const personData = await personRes.json();
+    const messageData = await messageRes.json();
+
+    response.render('index.liquid', {
+      persons: personData.data,
+      messages: messageData.data,
+      teamName: teamName
+    });
+  } catch (error) {
+    response.status(500).send("API Fout");
+  }
+});
+
+// ---------------------------------------------------------
+// POST /berichten
+// ---------------------------------------------------------
+app.post('/', async function (request, response) {
+  try {
+    await fetch(`${API_URL}/messages`, {
+      method: 'POST',
+      body: JSON.stringify({
+        for: `Team ${teamName}`,
+        from: request.body.from,
+        text: request.body.text
+      }),
+      headers: { 'Content-Type': 'application/json;charset=UTF-8' }
+    });
+
+    response.redirect(303, '/');
+  } catch (error) {
+    response.redirect(303, '/');
+  }
+});
+
+// ---------------------------------------------------------
+// GET /student/:id
+// ---------------------------------------------------------
+app.get('/student/:id', async function (request, response) {
+  try {
+    const personDetailRes = await fetch(`${API_URL}/person/${request.params.id}`);
+    const personDetailData = await personDetailRes.json();
+    
+    response.render('student.liquid', {
+      person: personDetailData.data
+    });
+  } catch (error) {
+  }
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Application started on http://localhost:${app.get('port')}`);
+});
