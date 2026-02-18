af3805b89d28efeadd1cadcc492a770e1f596687
TZGaming <tomzuidervaart@gmail.com>
2026-02-18 12:10:11 +0100

    Sidebar trigger

--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
+let personCards = document.querySelectorAll('.person-card')
 let chatContainer = document.querySelector('.chatContainer')
 let closeChat = document.querySelector('.closeChat')
 let openChatButton = document.querySelector('.openChat')
+let sideBar = document.querySelector('.person-sidebar-container')
 
 closeChat.addEventListener('click', () => {
     chatContainer.classList.add('hide')
@@ -11,3 +13,9 @@ openChatButton.addEventListener('click', () => {
     chatContainer.classList.remove('hide')
     openChatButton.classList.remove('show')
 })
+
+personCards.forEach(card => {
+    card.addEventListener('click', () => {
+        sideBar.classList.toggle('trigger-sidebar')
+    })
+});
