34e55b42a71cc1192459ac0ab27d1ff82ac73708
TZGaming <tomzuidervaart@gmail.com>
2026-02-18 12:35:52 +0100

    Page open animatie

--- public/script.js
+++ public/script.js
@@ -1,21 +1,27 @@
-let personCards = document.querySelectorAll('.person-card')
-let chatContainer = document.querySelector('.chatContainer')
-let closeChat = document.querySelector('.closeChat')
-let openChatButton = document.querySelector('.openChat')
-let sideBar = document.querySelector('.person-sidebar-container')
+let personCards = document.querySelectorAll('.person-card');
+let chatContainer = document.querySelector('.chatContainer');
+let closeChat = document.querySelector('.closeChat');
+let openChatButton = document.querySelector('.openChat');
+let sideBar = document.querySelector('.person-sidebar-container');
 
+// Chat sluiten
 closeChat.addEventListener('click', () => {
-    chatContainer.classList.add('hide')
-    openChatButton.classList.add('show')
-})
+    chatContainer.classList.add('hide');
+    openChatButton.classList.add('show');
+});
 
+// Chat openen
 openChatButton.addEventListener('click', () => {
-    chatContainer.classList.remove('hide')
-    openChatButton.classList.remove('show')
-})
+    chatContainer.classList.remove('hide');
+    openChatButton.classList.remove('show');
+});
 
-personCards.forEach(card => {
+// Kaarten animeren en sidebar triggeren
+personCards.forEach((card, index) => {
+    // Dynamische vertraging
+    card.style.animationDelay = `${.25 + (index * 0.05)}s`;
+
     card.addEventListener('click', () => {
-        sideBar.classList.toggle('trigger-sidebar')
-    })
+        sideBar.classList.toggle('trigger-sidebar');
+    });
 });
